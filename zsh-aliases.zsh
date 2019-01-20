bd950055ced8161dbec1610891ce8a2f59ba16d2
@slovacus <slovacus@gmail.com>
2019-01-20 12:52:28 -0500

    chore: functions pandoc and komiser (#5)

--- zsh-aliases.zsh
+++ zsh-aliases.zsh
@@ -34,5 +34,14 @@ if (( $+commands[docker] )); then
         docker run --rm -v $PWD:/data vimagick/youtube-dl
     }
 
+    function pandoc(){
+        docker run --rm -v $PWD:/data jpbernius/pandoc
+    }
 
+    function komiser(){
+        docker run --rm -d -p 3000:3000 \
+               -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+               -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+               -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION --name komiser mlabouardy/komiser
+    }
 fi
