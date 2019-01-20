dac77e8f853b54b7be61c40700e53de7bf9ebf4f
@slovacus <slovacus@gmail.com>
2019-01-20 12:34:56 -0500

    chore: implement info for zsh aliases (#5)

--- zsh-aliases.zsh
+++ zsh-aliases.zsh
@@ -1,11 +1,19 @@
 #!/usr/bin/env zsh
 # -*- coding: utf-8 -*-
 
+#
+# Defines alias for osx or linux.
+#
+# Authors:
+#   Luis Mayta <slovacus@gmail.com>
+#
+
 # Functions:
 # show nyancat in terminal
 function nyancat() {
     docker run -it --rm supertest2014/nyan;
 }
+
 # Alias for tmuxinator
 function mux() {
     tmuxinator ${1};
