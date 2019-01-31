dc95f10a9d1902edc0f53a4d980848343a1f6b92
@slovacus <slovacus@gmail.com>
2019-01-31 00:44:21 -0500

    (#7) Add: alias to pbcopy and pbpaste

--- zsh-aliases.zsh
+++ zsh-aliases.zsh
@@ -20,6 +20,23 @@ if (( $+commands[tmuxinator] )); then
         tmuxinator ${1};
     }
 fi
+
+# open, pbcopy and pbpaste on linux
+if [ "$(uname -s)" != "Darwin" ]; then
+	  if [ -z "$(command -v pbcopy)" ]; then
+		    if [ -n "$(command -v xclip)" ]; then
+			      alias pbcopy="xclip -selection clipboard"
+			      alias pbpaste="xclip -selection clipboard -o"
+		    elif [ -n "$(command -v xsel)" ]; then
+			      alias pbcopy="xsel --clipboard --input"
+			      alias pbpaste="xsel --clipboard --output"
+		    fi
+	  fi
+	  if [ -e /usr/bin/xdg-open ]; then
+		    alias open="xdg-open"
+	  fi
+fi
+
 if (( $+commands[docker] )); then
     # show nyancat in terminal
     function nyancat() {
