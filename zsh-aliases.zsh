99405a9d7f3520591460432cb0de5d2a85b47e9e
@slovacus <slovacus@gmail.com>
2019-02-05 23:13:45 -0500

    (#14) Add: functions docker for zsh

--- zsh-aliases.zsh
+++ zsh-aliases.zsh
@@ -46,5 +46,36 @@ if [ "$(uname -s)" != "Darwin" ]; then
 fi
 
 if (( $+commands[docker] )); then
-    source ./docker.zsh
+    terraform() {
+        docker run --rm -it \
+               -v "${HOME}/.aws:/root/.aws" \
+               -v "$(pwd):/root" \
+               hashicorp/terraform:light
+    }
+
+    # show nyancat in terminal
+    function nyancat() {
+        docker run -it --rm supertest2014/nyan;
+    }
+
+    function ytd-mp3() {
+        docker run --rm -v \
+               $PWD:/data vimagick/youtube-dl \
+               --extract-audio --audio-format mp3 -l
+    }
+
+    function ytdl(){
+        docker run --rm -v $PWD:/data vimagick/youtube-dl
+    }
+
+    function pandoc(){
+        docker run --rm -v $PWD:/data jpbernius/pandoc
+    }
+
+    function komiser(){
+        docker run --rm -d -p 3000:3000 \
+               -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+               -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+               -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION --name komiser mlabouardy/komiser
+    }
 fi
