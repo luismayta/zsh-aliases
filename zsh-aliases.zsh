4f7e07696e5efa8afb505809b1f96097bd0fac6c
@slovacus <slovacus@gmail.com>
2019-03-02 16:45:24 -0500

    fix: var environment to functions (#17)

--- zsh-aliases.zsh
+++ zsh-aliases.zsh
@@ -50,12 +50,18 @@ if (( $+commands[docker] )); then
     function awscli () {
         docker run --rm -it \
                -v "${HOME}/.aws:/root/.aws" \
+               -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
+               -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
+               -e AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" \
                nikovirtala/awscli:latest "$@"
     }
 
     function aws-shell () {
         docker run --rm -it \
                -v "${HOME}/.aws:/root/.aws" \
+               -e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
+               -e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
+               -e AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}" \
                nikovirtala/aws-shell:latest "$@"
     }
 
